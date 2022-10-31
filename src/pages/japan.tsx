bb49adb73e7a1d2c6f32f4b753da1a708b1c60c0
Manuel <manu.benitez1@hotmail.com>
2022-10-31 21:13:31 +0100

    refactor: album styles

--- src/pages/japan.tsx
+++ src/pages/japan.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Modal from "../components/modal/modal";
+import React, { useEffect, useState } from "react";
 import PhotoCard from "../components/photo-card/photo-card";
 import { IPhotoCard } from "../components/photo-card/photo-card.types";
 import { japanAlbumConstants } from "../constants/japan.constants";
@@ -7,30 +6,26 @@ import "./pages.scss";
 
 const Japan = () => {
   const [photos, setPhotos] = useState<IPhotoCard[]>(japanAlbumConstants);
-  const [isModalOpen, setModalOpen] = useState<boolean>();
-  const [modalImage, setModalImage] = useState<string>("");
-  const [vertical, setVertical] = useState<boolean | undefined>(false);
-  const handleModal = (imageUrl: string, vertical: boolean | undefined) => {
-    setModalImage(imageUrl);
-    setVertical(vertical);
-    setModalOpen(true);
-  };
+  const [source, setSource] = useState<string>();
+
+  useEffect(() => {
+    setSource(photos[0].imageUrl);
+  }, []);
+
   return (
-    <div className="album-container">
-      {photos.map((photo, index) => (
-        <PhotoCard
-          imageUrl={photo.imageUrl}
-          key={index}
-          onClick={() => handleModal(photo.imageUrl, photo.vertical)}
-        />
-      ))}
-      {isModalOpen && (
-        <Modal
-          onClose={() => setModalOpen(false)}
-          imageUrl={modalImage}
-          vertical={vertical}
-        />
-      )}
+    <div className="page-container">
+      <div className="page-hero-image-container">
+        <img src={source} alt="" className="page-hero-image" />
+      </div>
+      <div className="album-container">
+        {photos.map((photo, index) => (
+          <PhotoCard
+            imageUrl={photo.imageUrl}
+            key={index}
+            onClick={() => setSource(photo.imageUrl)}
+          />
+        ))}
+      </div>
     </div>
   );
 };

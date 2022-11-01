28601b66e0550c7d49c70d206287063e008cdbb5
Manuel <manu.benitez1@hotmail.com>
2022-11-01 21:43:11 +0100

    refactor: styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,8 @@ import heroImage1 from "../assets/New Zealand/Segundo Rollo/003_2A.jpg";
 import heroImage2 from "../assets/Japan/Segundo rollo/FH000034.JPG";
 import heroImage3 from "../assets/Austria/Innsbruck/Colors/imm004_4.jpg";
 import heroImage4 from "../assets/Spain/IMG_2492.JPG";
+import heroImage5 from "../assets/Japan/Primer rollo/FH000032.JPG";
+import heroImage6 from "../assets/Argentina/Segundo Rollo/000028.BMP";
 
 const Home = () => {
   return (
@@ -12,6 +14,8 @@ const Home = () => {
         <img src={heroImage2} alt="" className="hero-image"></img>
         <img src={heroImage3} alt="" className="hero-image"></img>
         <img src={heroImage4} alt="" className="hero-image"></img>
+        <img src={heroImage5} alt="" className="hero-image"></img>
+        <img src={heroImage6} alt="" className="hero-image"></img>
         <div className="hero-title">Digital Scrapbook of Analog Pictures</div>
       </div>
     </div>
